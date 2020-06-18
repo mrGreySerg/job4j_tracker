e8a7ff9509108b4e97b53ae2d426b9161e54d2a5
Sergei Grey <sergey@mail.ru>
2020-06-18 14:35:23 +0300

    2. Очередь с приоритетом на LinkedList[41670#265058]

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,29 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+            if (tasks.size() == 0) {
+                break;
+            }
+            if (element.getPriority() > task.getPriority()) {
+                break;
+            } else if (element.getPriority() < task.getPriority()) {
+                index++;
+                break;
+            }
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+
+}
