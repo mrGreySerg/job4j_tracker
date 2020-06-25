a86bb7a77f61b6d9ccdefc5c7c4236ec13a87ae0
sergei kustrin <sergei@mail.ru>
2020-06-25 22:35:35 +0300

    1. Уникальные задачи.[212653#265070] изменение 1.

--- src/test/java/ru/job4j/collection/FullSearchTest.java
+++ src/test/java/ru/job4j/collection/FullSearchTest.java
@@ -13,14 +13,14 @@ import static org.hamcrest.core.Is.is;
 public class FullSearchTest {
 
     @Test
-    public void whenAdd1231Then123() {
+    public void extractNumber() {
         List<Task> tasks = Arrays.asList(
                 new Task("1", "First desc"),
                 new Task("2", "Second desc"),
                 new Task("1", "Third desc")
         );
         Set<String> resultSet = FullSearch.extractNumber(tasks);
-        Set<String> expected = new HashSet<>(Arrays.asList("1", "2", "3"));
+        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
         assertThat(resultSet, is(expected));
     }
 
